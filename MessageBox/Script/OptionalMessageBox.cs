9bd628ed57493607267ca2938f964886300874ce
NeilKleistGao <neilkleistgao@163.com>
2020-02-21 13:25:35 +0800

    initial

---
+++ MessageBox/Script/OptionalMessageBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionalMessageBox : MonoBehaviour
+{
+    public Color selected_color = Color.yellow;
+    public float speed = 1.0f;
+    public KeyCode next_key, left_key, right_key;
+    public AudioClip effect;
+
+    private const int options_count = 4;
+
+    private Text[] options = new Text[options_count];
+    private bool[] avaliable = new bool[options_count];
+    private Text message = null, speaker = null;
+    private Image sp = null;
+    // Start is called before the first frame update
+
+    Text getAndClear(string name) 
+    {
+        Text temp = transform.Find(name).GetComponent<Text>();
+        temp.text = "";
+        return temp;
+    }
+
+    void Start()
+    {
+        message = getAndClear("Message");
+        speaker = getAndClear("Name");
+        sp = transform.Find("SP").GetComponent<Image>();
+        sp.sprite = null;
+
+        for (int i = 0; i < options_count; i++)
+        {
+            string name = "Option" + i.ToString();
+            options[i] = getAndClear(name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(next_key))
+        {
+            //TODO:
+        }
+        else if (Input.GetKeyUp(left_key))
+        {
+            //TODO:
+        }
+        else if (Input.GetKeyUp(right_key))
+        {
+            //TODO:
+        }
+    }
+}
---
+++ ScriptCreator/Script/ScriptCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ScriptCreator : EditorWindow
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [MenuItem("RPG/Add New Script _n")]
+    public static void addNewScript()
+    {
+        ScriptCreator.CreateInstance<ScriptCreator>().Show();
+    }
+}
