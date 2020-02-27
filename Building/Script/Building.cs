65ea186ad2e2f9296f66a5796360be0f02909a90
NeilKleistGao <neilkleistgao@163.com>
2020-02-27 12:58:19 +0800

    add building

---
+++ Building/Script/Building.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public float alpha = 0.85f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PolygonCollider2D[] colliders = transform.GetComponentsInChildren<PolygonCollider2D>();
+        foreach (PolygonCollider2D collider in colliders)
+        {
+            if (collider.isTrigger)
+            {
+                Transparent transparent = collider.gameObject.AddComponent<Transparent>();
+                transparent.alpha = alpha;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Building/Script/Transparent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparent : MonoBehaviour
+{
+    public float alpha;
+    private SpriteRenderer renderer;
+    private string player_name = "Player";
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        checkAndSetAlpha(collision.gameObject.name, alpha);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        checkAndSetAlpha(collision.gameObject.name, 1);
+    }
+
+    void checkAndSetAlpha(string name, float alp)
+    {
+        if (name == player_name)
+        {
+            Color temp = renderer.color;
+            renderer.color = new Color(temp.r, temp.g, temp.b, alp);
+        }
+    }
+}
